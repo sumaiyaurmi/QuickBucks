4e6a9f726472a72c1f5274e7a091160db6b7d7fa
Sumaiya Urmi <akthersumaiya689@gmail.com>
2024-07-17 21:31:33 +0600

    register form added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,45 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <></>
+    <div className="">
+    <div className="text-center ">
+      <h1 className="text-3xl md:text-4xl font-bold">Welcome Back, Log In</h1>
+      <p className="text-xl font-semibold">Hi, we are you glad you are back. <br /> Please login.</p>
+    </div>
+ <div className="bg-slate-50 rounded-xl mt-4 md:w-[500px] mx-auto p-10 text-center">
+ <form className=" ">
+ 
+  
+    <div className="form-control">
+<label className="label">
+  <span className="label-text">Email</span>
+</label>
+<input
+  type="email"
+  placeholder="email"
+  className="input input-bordered"
+  required
+/>
+</div>
+    <div className="form-control">
+<label className="label">
+  <span className="label-text">PIN</span>
+</label>
+<input
+  type="number"
+  placeholder=" 5 digit pin"
+  className="input input-bordered"
+  required
+/>
+</div>
+    <div className="form-control">
+<button type="submit " className=" m-4 btn  w-full md:w-1/2 mx-auto bg-blue-300">Sign In</button>
+<p>Don't have an Account? <Link to="/registration" className="text-blue-700 underline">Sign Up</Link> </p>
+                
+</div>
+    </form>
+ </div>
+  </div>
   );
 };
 
